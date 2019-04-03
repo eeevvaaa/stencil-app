a4f8e0e99c6ec08cd5096706f62a0d1aa5e43d4c
Eva Li <EvaLi@truckstop.com>
2019-04-03 16:53:54 -0500

    Update modal and style

--- src/components/my-modal/my-modal.tsx
+++ src/components/my-modal/my-modal.tsx
@@ -5,8 +5,12 @@ import { Component, Method, Element, Prop } from '@stencil/core';
     styleUrl: 'my-modal.scss'
 })
 export class MyModal {
+    buttons = ['okay', 'cancel']
+    showOptions = false;
+    // declare a reference to host the element
     @Element() modalEl: HTMLElement;
 
+    // exposed properties
     @Prop() title: string;
     @Prop() content: string;
 
@@ -16,11 +20,22 @@ export class MyModal {
       this.modalEl.style.display = 'block'; 
     }
 
+    showOptionsHandler() {
+        this.showOptions = true;
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.title}</h1>
                 <p>{this.content}</p>
+                <br />
+                <button onClick={this.showOptionsHandler}>Show Options</button>
+                <br />
+                {this.buttons.map(btn => (
+                    <button>{btn}</button>
+                ))}
+
             </div>
         )
     }
